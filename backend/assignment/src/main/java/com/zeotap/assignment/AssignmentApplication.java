13f09e3608e64617a5fb240c47ec0596bf203671
Yogendra Kumar Patel <pratapatel2003@gmail.com>
2026-02-16 17:01:38 +0530

    adding backend and frontend src

---
+++ backend/assignment/src/main/java/com/zeotap/assignment/AssignmentApplication.java
@@ -0,0 +1,15 @@
+package com.zeotap.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class AssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentApplication.class, args);
+	}
+
+}
---
+++ backend/assignment/src/main/java/com/zeotap/assignment/service/SeedDataService.java
@@ -0,0 +1,41 @@
+package com.zeotap.assignment.service;
+
+import com.zeotap.assignment.repository.IncidentRepository;
+import com.zeotap.assignment.repository.model.Incident;
+import com.zeotap.assignment.repository.model.Severity;
+import com.zeotap.assignment.repository.model.Status;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class SeedDataService {
+	
+	@Autowired
+	private IncidentRepository incidentRepository;
+	
+	@Async
+	public void addRandomDataToTable(){
+		Severity[] severity = Severity.values();
+		Status[] status = Status.values();
+		com.zeotap.assignment.repository.model.Service[] service = com.zeotap.assignment.repository.model.Service.values();
+		for(int i = 0; i < 200;i++){
+			Incident incident = new Incident();
+			incident.setSeverity(severity[new Random().nextInt(severity.length)]);
+			incident.setTitle("Title - Random Incident - " + i);
+			incident.setStatus(status[new Random().nextInt(status.length)]);
+			incident.setService(service[new Random().nextInt(service.length)]);
+			incidentRepository.save(incident);
+			try {
+				TimeUnit.MINUTES.sleep(1);
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+				throw new IllegalStateException("Seeding interrupted while waiting between inserts", e);
+			}
+		}
+		
+	}
+}
